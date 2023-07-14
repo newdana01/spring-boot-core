ff9e45783f1b021528b5227d124f1e51fb6b5d47
newdana01 <newdana01@gmail.com>
2023-07-14 16:12:26 +0900

    LogDemoController 클래스 추가

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject(); // 이 시점에 myLogger 생성
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("Controller Test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
